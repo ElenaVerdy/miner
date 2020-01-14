55f1de05b3bab90a8d675cff61e656c3f32d7f4e
ElenaVerdy <sahal9894@gmail.com>
2020-01-14 10:58:06 +0300

    .

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ const serverErrorTxt = "There has been a server error. Please reload the page an
 
 
 const pool = new Pool({
-    connectionString: "postgres://postgres:1234567@localhost:5432/minesweeper"
-    //connectionString: "postgres://postgres:postgres@localhost:5432/miner"
+    //connectionString: "postgres://postgres:1234567@localhost:5432/minesweeper"
+    connectionString: "postgres://postgres:postgres@localhost:5432/miner"
 });
 
 const teamplayField             = {width: 40, height: 20, num: 160};
@@ -544,7 +544,7 @@ app.post("/newgame", (req, res) => {
     game.gameId = game.singlePlayer ? undefined : multiplayer.createNewTable(req.userInfo, req.body.private).gameId;
 
     game.fieldSize = req.body.fieldSize;
-console.log("render shit")
+
     res.render(path.join("partitials", "gameField"), {
         game: game,
         player1: req.userInfo,
